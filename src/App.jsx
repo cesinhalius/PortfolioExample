ddcf768f9e0490581ce00f829ad718fc730bf3e8
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-02 18:07:54 -0300

    Adicionando os componentes para página

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import About from './components/about/Index'
+import Experience from './components/experience/Index'
+import Footer from './components/footer/Index'
 import Header from './components/header/Index'
+import Nav from './components/nav/Index'
+import Services from './components/services/Index'
+import Contact from './components/contact/Index'
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Contact/>
+      <Footer/>
     </>
   )
 }
