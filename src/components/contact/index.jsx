6e4dd6c88fb37920b9a7ef4d9894ef425c1b5dc5
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-02 15:47:21 -0300

    Adicionando todos os componentes da página

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style'
+
+const Index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Index
---
+++ src/components/nav/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style'
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
