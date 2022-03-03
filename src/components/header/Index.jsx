bb68485f4cacbaa4ee38a7823b4a9f0f2dab8611
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-03 18:16:53 -0300

    Implementando informações na pagina principal

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -3,7 +3,13 @@ import './style.css'
 
 const Index = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I`m</h5>
+        <h1>Cesar Augusto</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
   )
 }
 
