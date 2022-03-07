9124c44f011ec764a06f08fa8d7f2a682960510b
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-07 19:04:33 -0300

    Adicionando as imagems da lista de navegação

--- src/components/nav/Index.jsx
+++ src/components/nav/Index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './style.css'
-
+import { BiHome } from 'react-icons/bi'
+import { BiMessageSquareDetail } from 'react-icons/bi'
+import { FiUser } from 'react-icons/fi'
+import { ImBook } from 'react-icons/im'
+import { MdOutlineDesignServices } from 'react-icons/md'
 const Index = () => {
   return (
-    <div>Index</div>
+    <nav>
+      <a href="#"><BiHome/></a>
+      <a href="#about"><FiUser/></a>
+      <a href="#experience"><ImBook/></a>
+      <a href="#services"><MdOutlineDesignServices/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
