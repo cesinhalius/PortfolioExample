12c8b025d18a6f1282015ed4e4a2d3aa25521340
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-08 20:09:13 -0300

    Adicionando elementos na página e Adicionado estilo para a seção

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import './style.css'
-
+import React from "react";
+import "./style.css";
+import ME from "../../assets/about.jpg";
+import {FaAward} from 'react-icons/fa';
+import {FiUser} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
+ 
 const Index = () => {
   return (
-    <section id='about'>
-      About
-    </section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default Index
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="perfil" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>3+ Year Working</small>
+              </article>
+
+            <article className="about__card">
+                <FiUser className='about__icon'/>
+                <h5>Clients</h5>
+                <small>200+ Clients</small>
+              </article>
+
+            <article className="about__card">
+                <VscFolderLibrary className='about__icon'/>
+                <h5>Project</h5>
+                <small>80+ Complete</small>
+              </article>
+          </div>
+
+
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+          with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let`s Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Index;
