19fef5ccccb41f223935c8ba44d875b170df41c3
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-08 18:01:21 -0300

    Adicionando animação ao menu de navegação

--- src/components/nav/Index.jsx
+++ src/components/nav/Index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
 import { BiHome } from 'react-icons/bi'
 import { BiMessageSquareDetail } from 'react-icons/bi'
@@ -6,16 +6,18 @@ import { FiUser } from 'react-icons/fi'
 import { ImBook } from 'react-icons/im'
 import { MdOutlineDesignServices } from 'react-icons/md'
 
+
 const Index = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><BiHome/></a>
-      <a href="#about"><FiUser/></a>
-      <a href="#experience"><ImBook/></a>
-      <a href="#services"><MdOutlineDesignServices/></a>
-      <a href="#contact"><BiMessageSquareDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav ==='#about' ? 'active' : ''}><FiUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav ==='#experience' ? 'active' : ''}><ImBook/></a>
+      <a href="#service" onClick={() => setActiveNav('#service')} className={activeNav ==='#service' ? 'active' : ''}><MdOutlineDesignServices/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ==='#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
-}
+}  
 
 export default Index
