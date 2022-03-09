082fd2650562495a5e740e4ba2959cfaa38c081a
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-09 16:25:44 -0300

    Adicionando informações para a pagina  e estilo

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -35,17 +35,14 @@ const Index = () => {
             <article className="about__card">
                 <VscFolderLibrary className='about__icon'/>
                 <h5>Project</h5>
-                <small>80+ Complete</small>
+                <small>80 Complete</small>
               </article>
           </div>
 
 
           <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
-          specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-          with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla ex facere repellendus laudantium sequi 
+            ipsum sint dolore cupiditate? Dolor,non similique porro maxime iure vel. Sapiente exercitationem voluptatem nobis ipsum?
           </p>
 
           <a href="#contact" className="btn btn-primary">Let`s Talk</a>
