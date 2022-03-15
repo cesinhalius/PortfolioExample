90c34e65a7e451c6adfb24959965e6dc4849b279
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-15 17:45:56 -0300

    Adicionando elementos para comunicação

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -14,20 +14,26 @@ const Index = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact_option-icon'/>
             <h4>Email</h4>
             <h5>abcdefgh@contato.com</h5>
             <a href="#">Send Message</a>
           </article>
 
           <article className="contact_option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact_option-icon'/>
             <h4>Whatsapp</h4>
             <h5>+123456789</h5>
             <a href="#">Send Message</a>
           </article>
 
         </div>
+        <form action="">
+          <input type="text" name="name" placeholder='Full Name' required />
+          <input type="email" name="email" placeholder='abcdefghi@contato.com' required />
+          <textarea name="message" rows="7" placeholder='Message' required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
       </div>
     </section>
   )
