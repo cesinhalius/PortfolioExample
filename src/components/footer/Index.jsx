c0d70c1e32c944d18b11faba2ce38dea2d5fcf89
Cesar Augusto <cesinhalius@hotmail.com>
2022-03-15 19:05:47 -0300

    Adicionando elementos no rota pé da página

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './style.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 
-const Index = () => {
+export const Index = () => {
   return (
-    <div>Index</div>
+    <footer>
+        <ul className='permalinks'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experient">Experient</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer__socials">
+            <a href="https://facebook.com"><FaFacebook/></a>
+            <a href="https://instagram.com"><FaInstagram/></a>
+            <a href="https://twitter.com"><FaTwitter/></a>
+        </div>
+    </footer>
   )
 }
-
-export default Index
+export default Index;
